5ab0890329cd5f72f0e0777328c6dcc5cb202da2
Gabriel Agerholm Ruge <98162688+Dolphus2@users.noreply.github.com>
2026-01-10 13:17:26 +0100

    Decided to split dataset into to two dataset classes. One always returns tokens, the other always returns the text dictionary

--- src/datasets/gigaword.py
+++ src/datasets/gigaword.py
@@ -1,8 +1,9 @@
+from typing import Optional
 from src.datasets import BaseDataset
 from pathlib import Path
 import os
 import logging
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 from dotenv import load_dotenv
 from src.datasets.utils import get_tokenize_function
 
@@ -17,7 +18,11 @@ logger = logging.getLogger(__name__)
 class DGigawordDataset(BaseDataset):
     """Danish Gigaword dataset"""
 
-    def __init__(self, **kwargs):
+    def __init__(
+        self, 
+        size: Optional[int] = None, 
+        **kwargs
+    ):
         super().__init__()
         self.name = "danish-foundation-models/danish-gigaword"
 
@@ -27,25 +32,116 @@ class DGigawordDataset(BaseDataset):
             cache_dir=str(CACHE_DIR),
             token=HF_TOKEN,
             **kwargs,
-        )
+        )    
+        if size:
+            self.ds = self.ds.shuffle(seed=42).select(range(size))
         self.size = len(self.ds)
 
     def __len__(self) -> int:
         return self.size
 
     def __getitem__(self, index: int) -> dict:
-        return self.ds[index]['text']
+        return self.ds[index]
+
+    def _preprocess(self, checkpoint: str, num_proc: int = 4) -> Dataset:
+        tokenize_function = get_tokenize_function(checkpoint)
+
+        processed_ds = self.ds.map(
+            tokenize_function, 
+            batched=True,
+            num_proc=num_proc,
+        remove_columns=self.ds.column_names,
+        )
+        logger.info(f"Preprocessed dataset with {len(self.ds)} samples. Processed dataset has {len(processed_ds)} samples")
+        return processed_ds
+
+class TDGigawordDataset(DGigawordDataset):
+    """Tokenized Danish Gigaword dataset"""
+
+    def __init__(
+        self, 
+        checkpoint: Optional[str],
+        size: Optional[int] = None, 
+        preprocess: bool = False,
+        num_proc: int = 4,
+        **kwargs
+    ):
+        super().__init__(size, **kwargs)
+        self.checkpoint = checkpoint if checkpoint else "distilbert/distilgpt2"
+        self.tokenize_function = get_tokenize_function(checkpoint)
+        self.preprocessed = False
+
+        if preprocess:
+            if not checkpoint:
+                raise ValueError("checkpoint must be provided when preprocess=True")
+            self.preprocess(num_proc)
+
+
+    def __len__(self) -> int:
+        return self.size
+
+    def __getitem__(self, index: int) -> dict:
+        if self.preprocessed:
+            return self.ds[index]
+        else:
+            return self._labels_map(self.tokenize_function(self.ds[index]))
+
+    def _preprocess(self, num_proc: int = 4) -> Dataset:
+        assert self.preprocessed == False, "Called _preprocess but dataset is already preprocessed!"
+
+        processed_ds = self.ds.map(
+            self.tokenize_function, 
+            batched=True,
+            num_proc=num_proc,
+        remove_columns=self.ds.column_names,
+        ).map(self._labels_map)
+        logger.info(f"Preprocessed dataset with {len(self.ds)} samples. Processed dataset has {len(processed_ds)} samples")
+        return processed_ds
+
+    def preprocess(self, num_proc: int = 4):
+        """Public method to preprocess after initialization."""
+        if self.preprocessed:
+            logger.warning("Dataset already preprocessed. Skipping.")
+            return
+        
+        self.ds = self._preprocess(num_proc)
+        self.size = len(self.ds)
+        self.preprocessed = True
+    
+    @staticmethod
+    def _labels_map(elem):
+        # Set labels to input_ids
+        return elem | {"labels": elem["input_ids"]}
 
 
 if __name__ == "__main__":
-    dataset = DGigawordDataset()
+    # Standard DGigawordDataset returns dictionaries with text
+    dataset = DGigawordDataset(1000)
     print(len(dataset))
+    print(dataset.ds.features)
+    for i in range(10):
+        sample = dataset[i]
+        print(sample)
 
+    # TDGigawordDataset returns dictionaries with toekens and attention maps.
     checkpoint = "distilbert/distilgpt2"
-    tokenize_function = get_tokenize_function(checkpoint)
-    sample = tokenize_function(dataset[0])
-    print(sample)
-    for i in range(20):
-        sample = tokenize_function(dataset[i])
+    dataset = TDGigawordDataset(checkpoint, 1000)
+    print(dataset.ds.features)
+    for i in range(10):
+        sample = dataset[i]
         print(sample["input_ids"].shape)
         print(sample["attention_mask"].shape)
+    dataset.ds.column_names
+    
+    dataset = TDGigawordDataset(checkpoint, 1000, preprocess=True)
+    print(dataset.ds.features)
+
+    # from torch.utils.data import DataLoader, random_split, Dataset
+    # dl = DataLoader(dataset=dataset)
+    # for batch in dl:
+    #     print(batch)
+    #     break
+
+    # for i in range(20):
+    #     sample = next(dl)
+    #     print(sample)
--- src/datasets/utils.py
+++ src/datasets/utils.py
@@ -1,7 +1,8 @@
+from typing import Optional
 from pathlib import Path
 import os
 import logging
-from datasets import load_dataset, get_dataset_config_names
+from datasets import load_dataset, get_dataset_config_names, Dataset
 from dotenv import load_dotenv
 from transformers import AutoTokenizer
 
@@ -13,9 +14,16 @@ PROCESSED_DATA_PATH = Path("data") / "processed" / "danish_gigaword"
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+block_size = 128
 
-def preprocess(checkpoint: str, save_path=PROCESSED_DATA_PATH, **kwargs):
-    name = "danish-foundation-models/danish-gigaword"
+def preprocess(
+        name: str,
+        checkpoint: str, 
+        save_path=PROCESSED_DATA_PATH,
+        size: Optional[int] = None,
+        num_proc: int = 4,
+        **kwargs):
+        
     dataset = load_dataset(
         path=name,
         split="train",  # The dataset only has the trian split.
@@ -23,10 +31,18 @@ def preprocess(checkpoint: str, save_path=PROCESSED_DATA_PATH, **kwargs):
         token=HF_TOKEN,
         **kwargs,
     )
+    if size:
+        dataset = dataset.shuffle(seed=42).select(range(size))
     tokenize_function = get_tokenize_function(checkpoint)
-    tokenized_dataset = dataset.map(tokenize_function, batched=True)
-    save_path.mkdir(parents=True, exist_ok=True)
-    tokenized_dataset.save_to_disk(save_path)
+    tokenized_dataset = dataset.map(
+        tokenize_function, 
+        batched=True,
+        num_proc=num_proc,
+    remove_columns=dataset.column_names,
+    )
+    # lm_dataset = tokenized_dataset.map(lambda elem: group_texts(elem, block_size), batched=True, num_proc=4)
+    # save_path.mkdir(parents=True, exist_ok=True)
+    # tokenized_dataset.save_to_disk(save_path)
     return tokenized_dataset
 
 
@@ -34,23 +50,42 @@ def get_tokenize_function(checkpoint: str, **tokenizer_kwargs):
     """Create a tokenize function for the given checkpoint."""
 
     tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    tokenizer.pad_token = tokenizer.eos_token
 
     def tokenize_function(elem):
-        return tokenizer(elem, truncation=True, return_tensors="pt", **tokenizer_kwargs)
+        return tokenizer(elem["text"], padding=True, truncation=True, return_tensors="pt", **tokenizer_kwargs)
 
     return tokenize_function
 
+def group_texts(elem, block_size = 128):
+    concatenated_elems = {k: sum(elem[k], []) for k in elem.keys()}
+    total_length = len(concatenated_elems[list(elem.keys())[0]])
+    # We drop the small remainder
+    if total_length >= block_size:
+        total_length = (total_length // block_size) * block_size
+    # Split by chunks of block_size.
+    result = {
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_elems.items()
+    }
+    result["labels"] = result["input_ids"].copy()
+    return result
+
 
 if __name__ == "__main__":
-    name = "danish-foundation-models/danish-gigaword"
-    configs = get_dataset_config_names(name)
-    print(configs)
-    ds = load_dataset(
-        path=name,
-        split="train",  # The dataset only has the trian split.
-        cache_dir=str(CACHE_DIR),
-        token=HF_TOKEN,
-    )
+    # name = "danish-foundation-models/danish-gigaword"
+    # configs = get_dataset_config_names(name)
+    # print(configs)
+    # ds = load_dataset(
+    #     path=name,
+    #     split="train",  # The dataset only has the trian split.
+    #     cache_dir=str(CACHE_DIR),
+    #     token=HF_TOKEN,
+    # )
+    # print(ds.features)
+    # sample = ds[1]
+    # del ds
+    
+    # Load and preprocess the dataset
+    ds = preprocess("danish-foundation-models/danish-gigaword", "distilbert/distilgpt2", size=1000)
     print(ds.features)
-    sample = ds[1]
-    # print(sample)
