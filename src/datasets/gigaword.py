ed75074a484f7a48e280b5312dacde200a0890e5
Gabriel Agerholm Ruge <98162688+Dolphus2@users.noreply.github.com>
2026-01-10 19:51:00 +0100

    Refactored gigaword dataset to allow for loading the preprocessed dataset from disk

--- src/datasets/gigaword.py
+++ src/datasets/gigaword.py
@@ -3,11 +3,11 @@ from src.datasets import BaseDataset
 from pathlib import Path
 import os
 import logging
-from datasets import load_dataset, Dataset
+from datasets import load_dataset, load_from_disk, Dataset
 from dotenv import load_dotenv
 from src.datasets.utils import get_tokenize_function
 from transformers import AutoTokenizer
-from src import TensorDict
+from src import TensorDict, PathLike
 
 load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN") # Huggingface Token
@@ -22,11 +22,24 @@ class DGigawordDataset(BaseDataset):
     def __init__(
         self, 
         size: Optional[int] = None, 
+        skip_load: bool = False,
         **kwargs
     ):
         super().__init__()
         self.name = "danish-foundation-models/danish-gigaword"
+        self.kwargs = kwargs
+        self.dataset_loaded = False
+        # self.ds = None
+        if not skip_load:
+            self.load(size, **kwargs)
+        else:
+            self.ds = None
+            self.size = 0
 
+    def load(self, size, **kwargs):
+        if self.dataset_loaded:
+            logger.warning("Dataset already preprocessed. Skipping.")
+            return
         self.ds = load_dataset(
             path=self.name,
             split="train",  # The dataset only has the trian split.
@@ -37,6 +50,7 @@ class DGigawordDataset(BaseDataset):
         if size:
             self.ds = self.ds.shuffle(seed=42).select(range(size))
         self.size = len(self.ds)
+        self.dataset_loaded = True
 
     @property
     def features(self):
@@ -69,7 +83,7 @@ class DGigawordDataset(BaseDataset):
         logger.info(f"Preprocessed dataset with {len(self.ds)} samples. Processed dataset has {len(processed_ds)} samples")
         return processed_ds
     
-    
+
 
 class TDGigawordDataset(DGigawordDataset):
     """Tokenized Danish Gigaword dataset"""
@@ -79,23 +93,33 @@ class TDGigawordDataset(DGigawordDataset):
         checkpoint: Optional[str],
         size: Optional[int] = None, 
         preprocess: bool = False,
+        preprocessed_path: PathLike = None,
         num_proc: int = 4,
         **kwargs
     ):
-        super().__init__(size, **kwargs)
+        if preprocessed_path:
+            super().__init__(size=None, skip_load=preprocessed_path, **kwargs)
+            self.ds = load_from_disk(preprocessed_path, keep_in_memory=False)
+            self.ds.set_format("torch")
+            self.size = len(self.ds)
+            self.preprocessed = True 
+        else:
+            super().__init__(size, **kwargs)
+            self.preprocessed = False
+
         self.checkpoint = checkpoint if checkpoint else "distilbert/distilgpt2"
         self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
         self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.preprocessed = False
-
+        
         if preprocess:
-            if not checkpoint:
-                raise ValueError("checkpoint must be provided when preprocess=True")
-            # if size >= 1000: raise NotImplementedError("Preprocessing for large datasets currently causes OOM crashes.")
-            self.preprocess(num_proc)
+            if not preprocessed_path:
+                if not checkpoint:
+                    raise ValueError("checkpoint must be provided when preprocess=True")
+                self.preprocess(num_proc)
+            else:
+                logger.warning("Preprocessed dataset loaded. Skipping.")
 
     def tokenize_function(self, elem):
-        #return self.tokenizer(elem["text"], padding=True, truncation=True, return_tensors="pt")
         # Ensure elements are tokenized to the same length for batched and parrelilized operations.
         return self.tokenizer(elem["text"], padding='max_length', truncation=True)
 
@@ -148,7 +172,7 @@ if __name__ == "__main__":
         sample = dataset[i]
         print(sample)
 
-    # TDGigawordDataset returns dictionaries with tokens and attention maps.
+    # # TDGigawordDataset returns dictionaries with tokens and attention maps.
     checkpoint = "distilbert/distilgpt2"
     dataset = TDGigawordDataset(checkpoint, 1000)
     print(dataset.features)
@@ -156,8 +180,8 @@ if __name__ == "__main__":
         sample = dataset[i]
         print(sample["input_ids"].shape)
         print(sample["attention_mask"].shape)
-    
-    dataset = TDGigawordDataset(checkpoint, 10000, preprocess=True, num_proc=8)
+
+    dataset = TDGigawordDataset(checkpoint, 1000, preprocess=True, num_proc=8)
     print(dataset.features)
     for i in range(10):
         sample = dataset[i]
