6114472d92e4191e3d0c6a91e8c615d4b29eede7
Gabriel Agerholm Ruge <98162688+Dolphus2@users.noreply.github.com>
2026-01-10 15:15:49 +0100

    Updated Hestnet

--- src/networks/hestnet.py
+++ src/networks/hestnet.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from torch import Tensor
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -6,6 +7,7 @@ from pathlib import Path
 import os
 import hydra
 from omegaconf import DictConfig
+from transformers.modeling_outputs import CausalLMOutputWithCrossAttentions
 
 
 load_dotenv()
@@ -22,38 +24,58 @@ class HestNet(nn.Module):
     ):
         super().__init__()
         self.checkpoint = checkpoint
-        self.tokenizer = AutoTokenizer.from_pretrained(checkpoint, cache_dir=MODEL_DIR / checkpoint)
         self.model = AutoModelForCausalLM.from_pretrained(checkpoint, cache_dir=MODEL_DIR / checkpoint)
+        self.tokenizer = AutoTokenizer.from_pretrained(checkpoint, cache_dir=MODEL_DIR / checkpoint)
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+    
 
-    def tokenize_function(self, elem):
-        print(elem)
-        return self.tokenizer(elem, truncation=True, return_tensors="pt")
 
-    def _get_tokinizer(self):
-        return self.tokenizer
 
-    def forward(self, inputs: str, **kwargs) -> Tensor:
-        encoded_input = self.tokenize_function(inputs)  # contains "input_ids" and "attention_mask"
-        outputs = self.model(**encoded_input, labels=encoded_input["input_ids"], **kwargs)
+    # @property
+    # def model(self):
+    #     return self.model
+
+    # def tokenize_function(self, elem):
+    #     print(elem)
+    #     return self.tokenizer(elem, truncation=True, return_tensors="pt")
+
+    # def _get_tokinizer(self):
+    #     return self.tokenizer
+
+    def forward(self, inputs, **kwargs) -> CausalLMOutputWithCrossAttentions:
+        # encoded_input = self.tokenize_function(inputs)  # contains "input_ids", "attention_mask" and "labels"
+        outputs = self.model(**inputs, **kwargs)
         return outputs
+    #: dict[str: Tensor]
 
 
 @hydra.main(version_base=None, config_path="../../configs", config_name="config")
 def main(cfg: DictConfig) -> None:
     # Example
+    prompt = "Jeg bor i et kommodeskab "
     checkpoint = cfg.model.network.checkpoint
+    #tokenizer = AutoTokenizer.from_pretrained(checkpoint, cache_dir=MODEL_DIR / checkpoint)
     model = HestNet(checkpoint)
-    text = "Jeg bor i et kommodeskab"
-    outputs = model(text)
+    inputs = model.tokenizer(prompt, return_tensors="pt")
+    print(inputs)
+    outputs = model(inputs, labels=inputs["input_ids"])  
     print(f"Model: {checkpoint}")
-    print(f"Input: {text}")
     print(f"Loss: {outputs.loss.item():.4f}")
     print(f"Logits shape: {outputs.logits.shape}")
+    print(type(outputs))
+
+    outputs = model.model.generate(inputs.input_ids, attention_mask= inputs.attention_mask, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95)
+    response = model.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    print(response)
+    # answer_start_index = outputs.start_logits.argmax()
+    # answer_end_index = outputs.end_logits.argmax()
+    # predict_answer_tokens = inputs.input_ids[0, answer_start_index : answer_end_index + 1]
+    # tokenizer.decode(predict_answer_tokens, skip_special_tokens=True)
 
     # Equivalent example
     tokenizer = AutoTokenizer.from_pretrained(checkpoint, cache_dir=MODEL_DIR / checkpoint)
     model = AutoModelForCausalLM.from_pretrained(checkpoint, cache_dir=MODEL_DIR / checkpoint)
-    inputs = tokenizer("Jeg bor i et kommodeskab", return_tensors="pt")
+    inputs = tokenizer(prompt, return_tensors="pt")
     outputs = model(**inputs, labels=inputs["input_ids"])
     print(f"Logits shape: {outputs.logits.shape}")
 
