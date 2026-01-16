0d6f50476b2e2ad106cd5de5130ac7936e91b6eb
andreas-desktop <s214630@dtu.dk>
2026-01-16 16:21:14 +0100

    Refactor LLM configuration, update model checkpoint, and enhance BatchesPerSecondCallback functionality

--- src/callbacks/metrics.py
+++ src/callbacks/metrics.py
@@ -1,11 +1,14 @@
+from sentence_transformers import SentenceTransformer
 import torch
-from FlagEmbedding import BGEM3FlagModel
 from src.datasets import BaseTextDataset
 from torch import Tensor
 from pytorch_lightning import Callback
 from src.utils import temporary_seed
 from src.lightning_modules import CausalLLM
 import pytorch_lightning as pl
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class FrechetDistanceMetric(Callback):
@@ -15,19 +18,27 @@ class FrechetDistanceMetric(Callback):
     ):
         super().__init__()
         self.references = [reference_dataset[i]["text"] for i in range(len(reference_dataset))]
-        self.prompts = [""] * len(reference_dataset)  # empty prompts for unconditional generation
-        self.model = BGEM3FlagModel("BAAI/bge-m3", use_fp16=True)
+        # make prompts. they should consist of the first few words of each reference
+        # the model then test if the generated text from these prompts are similar to the references
+        self.prompts = [" ".join(ref.split()[:4]) for ref in self.references]
+
+        model_name = "Qwen/Qwen3-Embedding-0.6B"
+        self.model = SentenceTransformer(model_name)
+        self.model.eval()
+        num_params = sum(p.numel() for p in self.model.parameters())
+        logger.info(f"Loaded {model_name} model with {num_params} parameters for Frechet Distance Metric...")
         self.ref_stats = None
 
+    @torch.no_grad()
     def _get_stats(self, text: list[str]) -> tuple[Tensor, Tensor]:
         # limit the number of tokens to 100
         # why?
         # 1) efficiency
         # 2) to avoid sentence length bias
-        embeds = self.model.encode(text, batch_size=32, max_length=100)["dense_vecs"]
+        embeds = self.model.encode_document(text)
         embeds = torch.from_numpy(embeds)
-        mu = embeds.mean(dim=0)
-        sigma = torch.cov(embeds.T)
+        mu = torch.mean(embeds, dim=0).double()
+        sigma = torch.cov(embeds.T).double()
         return mu, sigma
 
     def _register_ref_stats(self):
@@ -41,11 +52,16 @@ class FrechetDistanceMetric(Callback):
         sigma_y: Tensor,
     ) -> float:
         a = (mu_x - mu_y).square().sum()
-        b = sigma_x.trace() + sigma_y.trace()
+        b = sigma_x.float().trace() + sigma_y.float().trace()
         c = torch.linalg.eigvals(sigma_x @ sigma_y).sqrt().real.sum()
         return (a + b - 2 * c).clamp(min=0.0).item()
 
+    def on_train_start(self, trainer: pl.Trainer, pl_module: CausalLLM):
+        self.model.to(pl_module.device)
+
     def on_validation_end(self, trainer: pl.Trainer, pl_module: CausalLLM):
+        logger = pl_module.logger
+
         if self.ref_stats is None:
             # compute and store reference stats only once since they are constant
             self._register_ref_stats()
@@ -57,7 +73,7 @@ class FrechetDistanceMetric(Callback):
 
         mu_gen, sigma_gen = self._get_stats(generations)
         fcd = self.frechet_distance(mu_gen, sigma_gen, mu_ref, sigma_ref)
-        pl_module.log("FrechetDistance", fcd)
+        logger.log_metrics({"FrechetDistance": fcd}, step=pl_module.global_step)
 
 
 if __name__ == "__main__":
