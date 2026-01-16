55158ad8fc7ae277fcd0b774716f82165d0af02c
andreas-desktop <s214630@dtu.dk>
2026-01-16 17:04:55 +0100

    Refactor code structure for improved readability and maintainability

--- src/callbacks/batches_per_second.py
+++ src/callbacks/batches_per_second.py
@@ -8,35 +8,43 @@ class BatchesPerSecondCallback(Callback):
     """
     A callback for logging the number of batches processed per second.
     This can be useful for monitoring training performance.
-    Logs both during training and validation phases.
+    Logs both during training, validation, and testing phases.
     """
 
     def __init__(self):
         # initialize time trackers
-        self.train_info = {"steps": 0, "avg": 0.0, "time": None, "log_key": "train_batches_per_second"}
-        self.val_info = {"steps": 0, "avg": 0.0, "time": None, "log_key": "val_batches_per_second"}
+        self.info = {
+            "train": {"steps": 0, "summed_time": 0.0, "last_time": None},
+            "val": {"steps": 0, "summed_time": 0.0, "last_time": None},
+            "test": {"steps": 0, "summed_time": 0.0, "last_time": None},
+        }
 
-    def _track_bps(self, phase: Literal["train", "val"], pl_module: pl.LightningModule):
-        info = self.train_info if phase == "train" else self.val_info
-
-        before = info["time"]
-        steps = info["steps"]
-        avg = info["avg"]
-
+    def _track_bps(self, phase: Literal["train", "val", "test"], pl_module: pl.LightningModule):
+        info = self.info[phase]
+        before = info["last_time"]
         now = time.time()
 
-        if before is not None:
-            elapsed = now - before
-            curr_bps = 1.0 / elapsed
-            updated_avg = (avg * steps + curr_bps) / (steps + 1)
-            pl_module.log(info["log_key"], curr_bps)
-            info["avg"] = updated_avg
-            info["steps"] += 1
+        if before is None:
+            # if this is the first batch, just set the time
+            info["last_time"] = now
+            return
 
-        info["time"] = now
+        elapsed = now - before
+        info["summed_time"] += elapsed
+        info["steps"] += 1
+
+        bps = info["steps"] / info["summed_time"]
+
+        pl_module.log(f"{phase}_batches_per_second", bps)
+        info["last_time"] = now
 
     def on_train_batch_start(self, trainer, pl_module, batch, batch_idx):
+        # this measures time between the start of batches, i.e. a full iteration
+        # could use almost any hook
         self._track_bps("train", pl_module)
 
     def on_validation_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx=0):
         self._track_bps("val", pl_module)
+
+    def on_test_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx=0):
+        self._track_bps("test", pl_module)
